187a0d049bffa5eaf64b773c047bc99c2b9f46cd
clr <ricec2@rpi.edu>
2011-07-05 22:31:17 -0700

    The source is now a pool forwarder.
    Long polling is not written. There is no db integration.
    No plugins. No pool hopping

--- pool.py
+++ pool.py
@@ -6,19 +6,40 @@ from jsonrpc import ServiceProxy
 from twisted.web import server, resource
 from twisted.internet import reactor
 
+access = ServiceProxy("http://19ErX2nDvNQgazXweD1pKrjbBLKQQDM5RY:x@mining.eligius.st:8337")
+
+def jsonrpc_getwork(data):
+    global access
+    if data == None or data == []:
+        v = access.getwork()
+    else: v = access.getwork(data)
+    return v
+
+
+
 class Simple(resource.Resource):
     isLeaf = True
     def render_GET(self, request):
-        return "<html>Hello, world!</html>"
+        return "<html></html>"
+
+    def render_POST(self, request):
+        rpc_request = json.load(request.content)
+        print rpc_request
+        #check if they are sending a valid message
+        if rpc_request['method'] != "getwork":
+            return json.dumps({'result':None, 'error':'Not supported', 'id':rpc_request['id']})
+
+
+        #Check for data to be validated
+        data = rpc_request['params']
+        data = jsonrpc_getwork(data)
+        response = json.dumps({"result":data,'error':None,'id':rpc_request['id']})
+        print response
+        return response
+
 
     def getChild(self,name,request):
         return self
-
-def jsonrpc_getwork():
-    access = ServiceProxy("http://securerpc:bitcoin4access@127.0.0.1:8332")
-    v = access.getwork()
-    print v
-    return v
 
 
 def main():
