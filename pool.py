a8e0370dc539bd76030953cca3d4a7788114261a
clr <ricec2@rpi.edu>
2011-07-05 21:43:40 -0700

    Got the python twisted server working.

--- pool.py
+++ pool.py
@@ -1,7 +1,32 @@
 #!/bin/python
 
+import json
 from jsonrpc import ServiceProxy
 
-access = ServiceProxy("http://securerpc:bitcoin4access@127.0.0.1:8332")
-print access.getinfo()
-print access.listrecievedbyaddress(1)
+from twisted.web import server, resource
+from twisted.internet import reactor
+
+class Simple(resource.Resource):
+    isLeaf = True
+    def render_GET(self, request):
+        return "<html>Hello, world!</html>"
+
+    def getChild(self,name,request):
+        return self
+
+def jsonrpc_getwork():
+    access = ServiceProxy("http://securerpc:bitcoin4access@127.0.0.1:8332")
+    v = access.getwork()
+    print v
+    return v
+
+
+def main():
+
+    site = server.Site(Simple())
+    reactor.listenTCP(8337, site)
+    reactor.run()
+
+if __name__ == "__main__":
+    main()
+
