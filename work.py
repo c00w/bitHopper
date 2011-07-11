9b72246f32dad42511374c1e890bf7520132fb5c
clr <ricec2@rpi.edu>
2011-07-11 00:18:47 -0700

    Threw in a bunch of try except blocks to isolate where the twisted.internet.connection.Timeout error is comming from

--- work.py
+++ work.py
@@ -53,61 +53,85 @@ class WorkProtocol(Protocol):
 
 @defer.inlineCallbacks
 def jsonrpc_lpcall(agent,server, url, update):
-    global i
-    request = json.dumps({'method':'getwork', 'params':[], 'id':i}, ensure_ascii = True)
-    i = i +1
-    
-    header = {'Authorization':["Basic " +base64.b64encode(server['user']+ ":" + server['pass'])], 'User-Agent': ['bitHopper'],'Content-Type': ['application/json'] }
-    d = agent.request('GET', "http://" + server['mine_address']+ url)
-    d.addErrback(lambda x: defer.returnValue(None))
-    body = yield d
-    update(body)
+    try:
+        global i
+        request = json.dumps({'method':'getwork', 'params':[], 'id':i}, ensure_ascii = True)
+        i = i +1
+        
+        header = {'Authorization':["Basic " +base64.b64encode(server['user']+ ":" + server['pass'])], 'User-Agent': ['bitHopper'],'Content-Type': ['application/json'] }
+        d = agent.request('GET', "http://" + server['mine_address']+ url)
+        d.addErrback(lambda x: defer.returnValue(None))
+        body = yield d
+        update(body)
+    except Exception, e:
+        print 'Caught, jsonrpc_lpcall'
+        print e
+        defer.returnValue(None)
 
 @defer.inlineCallbacks
 def jsonrpc_call(agent, server,data , set_lp):
     global i
-    request = json.dumps({'method':'getwork', 'params':data, 'id':i}, ensure_ascii = True)
-    i = i +1
-    
-    header = {'Authorization':["Basic " +base64.b64encode(server['user']+ ":" + server['pass'])], 'User-Agent': ['bitHopper'],'Content-Type': ['application/json'] }
-    d = agent.request('POST', "http://" + server['mine_address'], Headers(header), StringProducer(request))
-    response = yield d
-    header = response.headers
-    #Check for long polling header
-    if set_lp(None, True):
-        for k,v in header.getAllRawHeaders():
-            if k.lower() == 'x-long-polling':
-                set_lp(v[0])
-                break
+    try:
+        request = json.dumps({'method':'getwork', 'params':data, 'id':i}, ensure_ascii = True)
+        i = i +1
+        
+        header = {'Authorization':["Basic " +base64.b64encode(server['user']+ ":" + server['pass'])], 'User-Agent': ['bitHopper'],'Content-Type': ['application/json'] }
+        d = agent.request('POST', "http://" + server['mine_address'], Headers(header), StringProducer(request))
+        response = yield d
+        header = response.headers
+        #Check for long polling header
+        if set_lp(None, True):
+            for k,v in header.getAllRawHeaders():
+                if k.lower() == 'x-long-polling':
+                    set_lp(v[0])
+                    break
 
-    finish = Deferred()
-    response.deliverBody(WorkProtocol(finish))
-    body = yield finish
+        finish = Deferred()
+        response.deliverBody(WorkProtocol(finish))
+        body = yield finish
+    except Exception, e:
+        print 'Caught, jsonrpc_call insides'
+        print e
+        defer.returnValue(None)
 
     try:
         message = json.loads(body)
         value =  message['result']
         defer.returnValue(value)
-    except exceptions.ValueError, e:
+    except Exception, e:
         print "Error in json decoding, Server probably down"
         print body
         defer.returnValue(None)
 
 @defer.inlineCallbacks
 def jsonrpc_getwork(agent, server, data, j_id, request, new_server, set_lp):
-    work = yield jsonrpc_call(agent, server,data,set_lp)
+    try:
+        work = yield jsonrpc_call(agent, server,data,set_lp)
+    except Exception, e:
+            print 'caught, first response/writing'
+            print e
+            work = None
     i = 0
     while work == None and data != []:
         i += 1
         if i > 3:
             new_server(server)
-        work = yield jsonrpc_call(agent, server,data,set_lp)
-        time.sleep(0.1)
+        try:
+            work = yield jsonrpc_call(agent, server,data,set_lp)
+            time.sleep(0.1)
+        except Exception, e:
+            print 'caught, inner jsonrpc_call loop'
+            print e
+            continue
 
-    if work != None:
-        response = json.dumps({"result":work,'error':None,'id':j_id})
-    else:
-        new_server(server)
-        response = json.dumps({"result":none,'error':{"message':'Server isn't responding"},'id':j_id})
-    request.write(response)
-    request.finish()
+    try:
+        if work != None:
+            response = json.dumps({"result":work,'error':None,'id':j_id})
+        else:
+            new_server(server)
+            response = json.dumps({"result":none,'error':{"message':'Server isn't responding"},'id':j_id})
+        request.write(response)
+        request.finish()
+    except Exception, e:
+            print 'caught, Final response/writing'
+            print e
