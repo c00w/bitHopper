61b91f6541457834f6442a44d7e3f97630be931b
clr <ricec2@rpi.edu>
2011-07-11 14:48:24 -0700

    Split the passwords off to a seperate file

---
+++ password.py
@@ -0,0 +1,14 @@
+
+#SET THESE
+bclc_user  = "FSkyvM"
+bclc_pass = "xndzEU"
+mtred_user = 'scarium'
+mtred_pass = 'x'
+eligius_address = '1AofHmwVef5QkamCW6KqiD4cRqEcq5U7hZ'
+btcguild_user = 'c00w_test'
+btcguild_pass = '1234'
+bitclockers_user = 'flargle'
+bitclockers_pass = 'x'
+mineco_user = 'c00w.test'
+mineco_pass = 'x'
+#REALLY
