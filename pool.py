7865dbf8aadfef506887bc20768446196efe9b85
clr <ricec2@rpi.edu>
2011-07-11 14:51:15 -0700

    removed passwords from pool.py

--- pool.py
+++ pool.py
@@ -1,4 +1,4 @@
-#!/bin/python2.7
+#!/bin/env python
 #License#
 #bitHopper by Colin Rice is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 #Based on a work at github.com.
@@ -27,19 +27,7 @@ import urllib2
 from twisted.internet.protocol import Protocol
 from twisted.python import log
 
-#SET THESE
-bclc_user  = "FSkyvM"
-bclc_pass = "xndzEU"
-mtred_user = 'scarium'
-mtred_pass = 'x'
-eligius_address = '1AofHmwVef5QkamCW6KqiD4cRqEcq5U7hZ'
-btcguild_user = 'c00w_test'
-btcguild_pass = '1234'
-bitclockers_user = 'flargle'
-bitclockers_pass = 'x'
-mineco_user = 'c00w.test'
-mineco_pass = 'x'
-#REALLY
+from password import *
 
 def get_difficulty():
     req = urllib2.Request('http://blockexplorer.com/q/getdifficulty')
