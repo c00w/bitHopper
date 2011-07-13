e5c740a4890d2026fdf1b8ef79dc75982e68a3b9
clr <ricec2@rpi.edu>
2011-07-12 19:27:09 -0700

    reordered things so that hopefully we will trip DDOS protection on eligius less

--- work.py
+++ work.py
@@ -123,8 +123,8 @@ def jsonrpc_getwork(agent, server, data, j_id, request, new_server, set_lp):
     while work == None:
         new_server(server)
         try:
-            work = yield jsonrpc_call(agent, server,data,set_lp)
             time.sleep(0.1)
+            work = yield jsonrpc_call(agent, server,data,set_lp)
         except Exception, e:
             print 'caught, inner jsonrpc_call loop'
             print e
