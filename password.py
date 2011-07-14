1725e8e78b9bbb133f978ebeaef9ed5f19fd957f
clr <ricec2@rpi.edu>
2011-07-14 09:39:45 -0700

    Added arstechnica and moved items in the password file around to make it more obvious what to do.

--- password.py
+++ password.py
@@ -1,5 +1,8 @@
 
-#SET THESE
+#These are the passwords used for mining.
+#You should replace them with your own
+ars_user = 'c00w.default'
+ars_pass = 'fire'
 bclc_user  = "FSkyvM"
 bclc_pass = "xndzEU"
 mtred_user = 'scarium'
@@ -8,18 +11,22 @@ eligius_address = '1AofHmwVef5QkamCW6KqiD4cRqEcq5U7hZ'
 btcguild_user = 'c00w_test'
 btcguild_pass = '1234'
 btcguild_user_apikey = '81a9081f4d5f32db7dbbed407cb5f686'
-bitclockers_user = 'flargle'
-bitclockers_pass = 'x'
-bitclockers_user_apikey = '965469e512aa0de5a37b83ccfd5e849e'
-mineco_user = 'c00w.test'
-mineco_pass = 'x'
 eclipsemc_user = 'c00w_worker'
 eclipsemc_pass = 'x'
 eclipsemc_apikey = 'bd0eded7e35b1b5fc48f8d10214d98'
-miningmainframe_user = 'c00w.1'
-miningmainframe_pass = 'x'  
-#pool.bitp.it
 bitp_user = '4bjibc3R1RGF9'
 bitp_pass= 'u5cpmndZq4Jvp'
 bitp_user_apikey = '80c5dbea750563eb24e5310f94a56b38bad14dc35efb41e9ca421c92f31563ed'
-#REALLY
+
+#These passswords are not currently used.
+#So if you have them replace them with your own. Otherwise don't worry
+
+mineco_user = 'c00w.test'
+mineco_pass = 'x'
+
+bitclockers_user = 'flargle'
+bitclockers_pass = 'x'
+bitclockers_user_apikey = '965469e512aa0de5a37b83ccfd5e849e'
+
+miningmainframe_user = 'c00w.1'
+miningmainframe_pass = 'x'  
