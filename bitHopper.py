0de92f23d0ed993837be21b4b67ec6b1dc503a82
clr <ricec2@rpi.edu>
2011-08-17 15:57:42 -0500

    Updates get_new_server to hopefully give us more info when it crashes

--- bitHopper.py
+++ bitHopper.py
@@ -117,7 +117,7 @@ class BitHopper():
         return
 
     def get_new_server(self, server):
-        self.pool.get_entry(server)['lag'] = True
+        self.pool.servers[server]['lag'] = True
         self.log_dbg('Lagging. :' + server)
         self.server_update()
         return self.pool.get_current()
