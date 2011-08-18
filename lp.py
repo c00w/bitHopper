e8f97be3ed9d1e5bfcd5e0f9bc34cb52b6ead13c
clr <ricec2@rpi.edu>
2011-08-18 13:56:08 -0500

    Check that the server is in our dictionary before checking its role

--- lp.py
+++ lp.py
@@ -45,7 +45,7 @@ class LongPoll():
             self.blocks[block]['_defer'] = defer.Deferred()
             self.bitHopper.log_msg('Setting Block Owner ' + server+ ':' + str(block))
 
-        if self.bitHopper.pool.servers[server]['role'] == 'mine_deepbit' and old_owner != server:
+        if server in self.bitHopper.pool.servers and self.bitHopper.pool.servers[server]['role'] == 'mine_deepbit' and old_owner != server:
             old_shares = self.bitHopper.pool.servers[server]['shares']
             self.bitHopper.pool.servers[server]['shares'] = 0
             self.bitHopper.select_best_server()
