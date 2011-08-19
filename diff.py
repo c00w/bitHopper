a764997238a70c5ee211e3200ac0848e0b8d5507
clr <ricec2@rpi.edu>
2011-08-19 14:51:15 -0400

    Made diff.py threadsafe

--- diff.py
+++ diff.py
@@ -5,6 +5,8 @@
 
 import urllib2
 import re
+import threading
+
 from twisted.internet.task import LoopingCall
 
 class Difficulty():
@@ -15,41 +17,47 @@ class Difficulty():
         self.nmc_difficulty = 94037.96
         self.ixc_difficulty = 16384
         self.i0c_difficulty = 4096
+        self.lock = threading.RLock()
         call = LoopingCall(self.update_difficulty)
         call.start(60*60*6)
 
     def get_difficulty(self):
-        return self.difficulty
+        with self.lock:
+            return self.difficulty
     
     def get_nmc_difficulty(self):
-        return self.nmc_difficulty
+        with self.lock:
+            return self.nmc_difficulty
 
     def get_ixc_difficulty(self):
-        return self.ixc_difficulty
+        with self.lock:
+            return self.ixc_difficulty
     
     def get_i0c_difficulty(self):
-        return self.i0c_difficulty
+        with self.lock:
+            return self.i0c_difficulty
 
     def update_difficulty(self):
         "Tries to update difficulty from the internet"
-        self.bitHopper.log_msg('Updating Difficulty')
-        try:
-            req = urllib2.Request('http://blockexplorer.com/q/getdifficulty')
-            response = urllib2.urlopen(req)
-            diff_string = response.read()
-            self.difficulty = float(diff_string)
-            self.bitHopper.log_msg(str(diff_string))
-        except:
-            pass
-        
-        self.bitHopper.log_msg('Updating NameCoin Difficulty')
-        try:
-            req = urllib2.Request('http://namebit.org/')
-            response = urllib2.urlopen(req)
-            diff_str = response.read()
-            output = re.search('<td id="difficulty">([.0-9]+)</td>', diff_str)
-            output = output.group(1)
-            self.nmc_difficulty = float(output)
-            self.bitHopper.log_msg(str(self.nmc_difficulty))
-        except:
-            pass
+        with self.lock:
+            self.bitHopper.log_msg('Updating Difficulty')
+            try:
+                req = urllib2.Request('http://blockexplorer.com/q/getdifficulty')
+                response = urllib2.urlopen(req)
+                diff_string = response.read()
+                self.difficulty = float(diff_string)
+                self.bitHopper.log_msg(str(diff_string))
+            except:
+                pass
+            
+            self.bitHopper.log_msg('Updating NameCoin Difficulty')
+            try:
+                req = urllib2.Request('http://namebit.org/')
+                response = urllib2.urlopen(req)
+                diff_str = response.read()
+                output = re.search('<td id="difficulty">([.0-9]+)</td>', diff_str)
+                output = output.group(1)
+                self.nmc_difficulty = float(output)
+                self.bitHopper.log_msg(str(self.nmc_difficulty))
+            except:
+                pass
