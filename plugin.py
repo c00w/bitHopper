cb1e7423998e2120be485bc5318e1514cf5beb73
sdogi <sdogic@gmail.com>
2011-09-04 11:35:26 +0300

    Made plugin system fail to auto mode when it can't find anything from bh.cfg. In manual mode though it will only activate plugin when it can find pluginname = True from [plugins] section.
    Also made plugin system print prettier messages to make it easier to debug plugin issues in the future.

--- plugin.py
+++ plugin.py
@@ -33,37 +33,51 @@ class Plugin():
     def load_plugins(self):
         """Tries to load all plugins in folder plugins"""
         config = self.bitHopper.config
-        if config.get('pluginmode', 'mode').find('disabled') == -1:
-            self.bitHopper.log_msg('Loading Plugins')
-            possible_plugins = os.listdir('plugins')
-
-            if config.get('pluginmode', 'mode').find('auto') > -1:
-                autoMode = True
+        try:
+            if config.get('pluginmode', 'mode').find('disabled') == -1:
+                if config.get('pluginmode', 'mode').find('auto') > -1:
+                    autoMode = True
+                    self.bitHopper.log_msg("Plugin loading mode: auto")
+                else:
+                    self.bitHopper.log_msg("Plugin loading mode: manual")
+                    autoMode = False
             else:
-                autoMode = False
-
-            for item in possible_plugins:
-                if os.path.isdir(os.path.join('plugins', item)):
+                self.bitHopper.log_msg("Plugin loading mode: disabled")
+                return
+        except:
+            self.bitHopper.log_dbg('Unable to find [pluginmode] section from bh.cfg, running with auto plugin loading mode')
+            autoMode = True
+        self.bitHopper.log_msg('Loading Plugins')
+        possible_plugins = os.listdir('plugins')
+        for item in possible_plugins:
+            if os.path.isdir(os.path.join('plugins', item)):
+                if autoMode:
+                    pluginEnabled = True
+                else:
+                    pluginEnabled = False
                     try:
-                        if config.getboolean('plugins', item) or autoMode:
-                            try:
-                                module = importlib.import_module('plugins.' + str(item))
-                                bithop_attr = getattr(self.bitHopper, item, None)
-                                if bithop_attr is not None:
-                                    self.bitHopper.log_msg('Plugin name conflict: ' + str(item))
-                                    continue
-                                
-                                #Actually call module and store it in bitHopper
-                                return_value = module.main(self.bitHopper)
-                                if return_value is None:
-                                    setattr(self.bitHopper, item, module)
-                                else:
-                                    setattr(self.bitHopper, item, return_value)
-                                self.bitHopper.log_msg("Plugin: " + item + " loaded")
-                            except Exception, e:
-                                self.bitHopper.log_msg("ERROR LOADING PLUGIN: " + item)
-                                self.bitHopper.log_msg(e)
+                        if config.getboolean('plugins', item):
+                            pluginEnabled = True
                     except Exception, e:
-                        self.bitHopper.log_dbg('Unable to get boolean state for plugin ' + item + ': ' + str(e))
+                        self.bitHopper.log_msg("Plugin: " + item + " failed reading main config file: " + str(e))
                         pass
-
+                if pluginEnabled:
+                    try:
+                        module = importlib.import_module('plugins.' + str(item))
+                        bithop_attr = getattr(self.bitHopper, item, None)
+                        if bithop_attr is not None:
+                            self.bitHopper.log_msg('Plugin: name conflict: ' + str(item))
+                            continue
+                        
+                        #Actually call module and store it in bitHopper
+                        return_value = module.main(self.bitHopper)
+                        if return_value is None:
+                            setattr(self.bitHopper, item, module)
+                        else:
+                            setattr(self.bitHopper, item, return_value)
+                        self.bitHopper.log_msg("Plugin: " + item + " loaded")
+                    except Exception, e:
+                        self.bitHopper.log_msg("Plugin: ERROR LOADING PLUGIN: " + item)
+                        self.bitHopper.log_msg(e)
+                else:
+                    self.bitHopper.log_msg("Plugin: " + item + " has been disabled")
