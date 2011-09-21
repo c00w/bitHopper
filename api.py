ce4c83589ce37241e1cfc94b2f7aafe792397a5c
clr <ricec2@rpi.edu>
2011-09-20 22:45:55 -0400

    Turn of tracebacks

--- api.py
+++ api.py
@@ -95,7 +95,7 @@ class API():
 
     def errsharesResponse(self, error, server_name):
         self.bitHopper.log_msg(server_name + ' api error:' + str(error))
-        traceback.print_exc()
+        #traceback.print_exc()
         pool = server_name
         self.servers[pool]['err_api_count'] += 1
         self.servers[pool]['init'] = True
