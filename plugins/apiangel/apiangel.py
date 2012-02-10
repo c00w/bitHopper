109c24887aa897f5c3b6d601fcbb69aa4938d2b1
Colin Rice <dah4k0r@gmail.com>
2012-02-10 15:46:31 -0500

    Update apiangel copyright notice

--- plugins/apiangel/apiangel.py
+++ plugins/apiangel/apiangel.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-#License#
-#bitHopper by Colin Rice is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
-#Based on a work at github.com.
+#Copyright (C) 2011,2012 Colin Rice
+#This software is licensed under an included MIT license.
+#See the file entitled LICENSE
+#If you were not provided with a copy of the license please contact: 
+# Colin Rice colin@daedrum.net
 
 import traceback, logging
 import gevent
