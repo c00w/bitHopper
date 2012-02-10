20e2c478774fe1f7dbf74669ee216ff81ff7323a
Colin Rice <dah4k0r@gmail.com>
2012-02-10 15:53:00 -0500

    More copyright cleanup. See earlier explanation

--- plugins/payouts/payouts.py
+++ plugins/payouts/payouts.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
-#License#
-# payouts.py by Cutmaster Flex and licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
-# Based on a work at github.com.
+#Copyright (C) 2011,2012 Colin Rice
+#This software is licensed under an included MIT license.
+#See the file entitled LICENSE
+#If you were not provided with a copy of the license please contact: 
+# Colin Rice colin@daedrum.net
+
+
 #
 # Usage: Add a unique wallet:xxxxx option for each pool to user.cfg
 #        this will overwrite any previous manually set payout value
