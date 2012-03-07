3775effd623c4a176f7470e38dd2282262c51bf3
Colin Rice <colin@daedrum.net>
2012-03-06 20:35:13 -0500

    Fix threading to work

--- Workers.py
+++ Workers.py
@@ -42,7 +42,7 @@ class Workers():
         
     def poll_thread(self):
         """
-        Thread that checks for new workers and writes the to the file
+        Thread that checks for new workers and writes them to the file
         """
         self.fd = None
         with self.lock:
@@ -52,6 +52,10 @@ class Workers():
             self.parser.read('worker.cfg')
             for item in self.parser.sections():
                 self.workers[item] = self.parser.items(item)
+                self.workers_lock[item] = {}
+                
+                for work, passw in self.workers[item]:
+                    self.worker_locks[item][(work, passw)] = threading.Semaphore(2)
                 
         while True:
             self.queue.get()
@@ -89,7 +93,7 @@ class Workers():
             for pool in self.workers:
                 for worker_tuple in self.worker_lock[pool]:
                     if self.worker_locks[pool][worker_tuple].acquire(False):
-                        return worker_tuple[0]. worker_tuple[1], None
+                        return worker_tuple[0], worker_tuple[1], None
                         
             self._release()
             gevent.sleep(0)
@@ -115,11 +119,11 @@ class Workers():
         if pool not in self.workers_lock:
             self.workers_lock[pool] = {}
             for item in self.workers[pool]:
-                self.workers_lock[pool][item] = threading.lock()
+                self.workers_lock[pool][item] = threading.Semaphore(2)
                 
         self.parser.set(pool, worker, password)
         self.workers[pool].append((worker, password))
-        self.workers_lock[pool][(worker, password)] = threading.lock()
+        self.worker_locks[pool][(worker, password)] = threading.Semaphore(2)
         
         self._release()
         self.queue.put(None, False)
