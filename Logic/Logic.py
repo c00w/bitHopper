2e2bd5228ad48cde5d2848fde5abbff36a8426f1
Colin Rice <colin@daedrum.net>
2012-03-10 17:36:59 -0500

    Logic modules is finished except for mine_c pools

--- Logic/Logic.py
+++ Logic/Logic.py
@@ -4,6 +4,7 @@ File implementing the actuall logic for the business side
 
 from btcnet_wrapper import btcnet_info
 import Workers
+import gevent
 
 class Logic():
     """
@@ -12,9 +13,26 @@ class Logic():
     
     def __init__(self):
         self.i = 1
+        self._server = set()
+        gevent.spawn(self.generate_servers)
+        gevent.sleep(0)
         
     def self.difficulty_cutoff(self, source):
-        diff = btcnet_info.get_coins(
+        """
+        returns the difficulty cut off for the pool
+        """
+        diff = btcnet_info.get_difficulty(source.coin)
+        diff = float(diff)
+        
+        #Propositional Hopping
+        if source.payout_scheme in ['prop']:
+            return float(diff) * 0.435
+            
+        #Score Hopping
+        if source.payout_scheme in ['score']:
+            # Incorrect method. Just using it for now.
+            # TODO FIX IT TO USE MINE_C CONSTANTS 
+            return float(diff) * 0.435
         
     def valid_scheme(self, source):
         """
@@ -31,7 +49,7 @@ class Logic():
             if source.payout_scheme.lower() in ['pps', 'smpps', 'pplns']
                 yield source
             
-            if source.payout_scheme.lower() in ['prop']:
+            if source.payout_scheme.lower() in ['prop', 'score']:
             
                 #Check if we have a share count
                 shares = source.shares
@@ -81,7 +99,10 @@ class Logic():
             if pool.payout_scheme.lower() in ['pplns', 'smpps', 'pps']:
                 yield pool
             
-    def find_best(source):
+    def filter_best(source):
+        """
+        returns the best pool or pools we have
+        """
         pools = [x for x in source]
         
         #See if we have a score or a prop pool
@@ -91,16 +112,26 @@ class Logic():
             min_ratio = min(float(pool.shares) / self.difficulty_cutoff(pool) for pool in hoppable)
             for pool in hoppable:
                 if float(pool.shares) / self.difficulty_cutoff(pool) = min_ratio:
-                    return pool    
+                    return set(pool)    
         
         #Select a backup pool
         backup = list(self.filter_secure(pools))
         if backup:
-            return self.select(backup)
+            return set(backup)
             
         raise ValueError("No valid pools configured")
         
+    def generate_servers(self):
+        """
+        Method that generate the best server
+        """
+        while True:
+            self._servers = list(self.filter_best(self.valid_scheme(self.valid_credentials(btcnet_info.get_pools()))))
+            gevent.sleep(30)
+            
+        
+    def _select(pools):
+        return pools[self.i % len(pools)]
+        
     def get_server(self):
-        servers = self.valid_scheme(self.valid_credentials(btcnet_info.get_pools()))
-        server = self.find_best(servers)
-        return server
+        return self._select(self._servers)
