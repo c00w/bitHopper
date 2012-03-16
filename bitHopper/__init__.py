8eec02875094f66c8e1d2ab2d529bbc1623b1d78
Colin Rice <colin@daedrum.net>
2012-03-16 17:10:05 -0400

    Start on networking
    Also added a prototype monkey patch to make httplib faster

--- bitHopper/__init__.py
+++ bitHopper/__init__.py
@@ -2,4 +2,10 @@ import gevent.monkey
 #Not patching thread so we can spin of db file ops.
 gevent.monkey.patch_all(thread=False, time=False)
 
+#Monkey Patch httplib2
+#import geventhttpclient.httplib
+#geventhttpclient.httplib.patch()
+import httplib2
+
 import Logic
+import Network
