9d3a5236b51f92cd262ab30d1a252f6cc0c580f1
Colin Rice <colin@daedrum.net>
2012-03-18 07:23:07 -0400

    More module setup and deleted a bunch of extraneous files

--- bitHopper/btcnet_wrapper.py
+++
@@ -1,7 +0,0 @@
-#Copyright (C) 2011,2012 Colin Rice
-#This software is licensed under an included MIT license.
-#See the file entitled LICENSE
-#If you were not provided with a copy of the license please contact: 
-# Colin Rice colin@daedrum.net
-
-import btcnet_info
