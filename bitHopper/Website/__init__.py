c9b47b8fee0a069517485b8d2bf1ab779a03369b
Colin Rice <colin@daedrum.net>
2012-03-23 23:47:27 -0400

    added a random secret key

--- bitHopper/Website/__init__.py
+++ bitHopper/Website/__init__.py
@@ -4,10 +4,14 @@ import bitHopper.util
 import bitHopper.Network
 
 import flask
-
+import random
 app = flask.Flask(__name__, template_folder='bitHopper/templates', 
             static_folder = 'bitHopper/static')
 app.Debug = False
+
+# Set a secret key. Mainly used for CSRF. Should be random otherwise 
+# no protection since no one is going to remember to change this
+app.secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(20))
 
 @app.teardown_request
 def teardown_request_wrap(exception):
