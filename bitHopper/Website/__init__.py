47f13b2d5e81b189a7c06d5bddae7ff15f69431a
Colin Rice <colin@daedrum.net>
2012-03-24 22:10:25 -0400

    Fixed some things

--- bitHopper/Website/__init__.py
+++ bitHopper/Website/__init__.py
@@ -21,31 +21,4 @@ def teardown_request_wrap(exception):
     if exception:
         logging.error(traceback.format_exc())
         return json.dumps({"result":None, 'error':{'message':'Invalid request'}, 'id':1})
-            
-@app.route("/", methods=['POST'])
-@app.route("/mine", methods=['POST','GET'])
-def mine():
-    """
-    Function that does basic handling of work requests
-    """
-    try:
-        rpc_request = json.loads(request.data)
-    except ValueError, e:
-        return bitHopper.util.error_rpc()
-        
-    #Check for valid rpc_request
-    if not bitHopper.util.validate_rpc(rpc_request):
-        return bitHopper.util.error_rpc()
-        
-    #If getworks just feed them data
-    if rpc_request['params'] == []:
-        #TODO, pass in headers
-        content, headers = bitHopper.Network.get_work()
-    
-    #Otherwise submit the work unit
-    else:
-        content, headers = bitHopper.Network.submit_work(rpc_request)
-        
-    return content
-        
-    
+
