c75b5cf19eec09de208e98c246f9f05fbbb759d8
Colin Rice <colin@daedrum.net>
2012-03-29 19:20:08 -0400

    Version increment

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "dummy_bithopper_install",
-    version = "0.0.4",
+    version = "0.0.5",
     description = ("A dummy package to install things correctly for bitHopper"),
     install_requires=[
 		'setuptools',
