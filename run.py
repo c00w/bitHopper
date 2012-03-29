3d5d756ca7c2cba828302312269274f4fc8b994e
Colin Rice <colin@daedrum.net>
2012-03-29 19:46:37 -0400

    Let run.py take options for the network setup

--- run.py
+++ run.py
@@ -1,8 +1,31 @@
 import bitHopper
-bitHopper.setup_miner()
-bitHopper.setup_control()
 import gevent
+import argparse
 
-while True:
-    gevent.sleep(100)
+def parse_config():
+    parser = argparse.ArgumentParser(description='Process bitHopper CommandLine Arguments')
+    parser.add_argument('--mine_port', metavar='mp', type=int, 
+                    default=8337, help='Mining Port Number')
+                   
+    parser.add_argument('--config_port', metavar='cp', type=int, 
+                    default=8339, help='Configuration Port Number')
+                    
+    parser.add_argument('--mine_localname', metavar='cp', type=str, 
+                    default='', help='Dns name to bind to')
+                    
+    parser.add_argument('--config_localname', metavar='cp', type=str, 
+                    default='', help='Dns name to bind to')
+                    
+    args = parser.parse_args()
+    return args
+    
+
+if __name__ == "__main__":
+
+    args = parse_config()
+    bitHopper.setup_miner(port = args.mine_port, host=args.mine_localname)
+    bitHopper.setup_control(port = args.config_port, host=args.config_localname)
+
+    while True:
+        gevent.sleep(100)
 
