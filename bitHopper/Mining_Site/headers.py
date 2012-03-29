afac657eaed3ad707241ebec7c77ca894546327f
Colin Rice <colin@daedrum.net>
2012-03-29 19:29:32 -0400

    Move mining_site headers to their own location

---
+++ bitHopper/Mining_Site/headers.py
@@ -0,0 +1,36 @@
+def clean_headers_client(header):
+    """
+    Only allows through headers which are safe to pass to the server
+    """
+    valid = ['user_agent', 'x-mining-extensions', 'x-mining-hashrate']
+    for name, value in header.items():
+        if name.lower() not in valid:
+            del header[name]
+        else:
+            del header[name]
+            header[name.lower()] = value
+    return header
+    
+def clean_headers_server(header):
+    """
+    Only allows through headers which are safe to pass to the client
+    """
+    valid = ['content-length', 'content-type', 'x-roll-ntime', 'x-reject-reason', 'noncerange']
+    for name, value in header.items():
+        if name.lower() not in valid:
+            del header[name]
+        else:
+            del header[name]
+            header[name.lower()] = value
+    return header
+        
+    
+def _get_headers(environ):
+    """
+    Returns headers from the environ
+    """
+    headers = {}
+    for name in environ:
+        if name[0:5] == "HTTP_":
+            headers[name[5:]] = environ[name]
+    return headers
