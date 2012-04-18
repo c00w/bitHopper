ad25ac6208b25571c5389b2792a10d999aec790a
Colin Rice <colin@daedrum.net>
2012-04-18 12:27:39 -0400

    use a list so we lock from the front first

--- bitHopper/Network/ResourcePool.py
+++ bitHopper/Network/ResourcePool.py
@@ -28,7 +28,7 @@ class ResourceGenerator:
         (lock, item) = (threading.Lock(), self.generate(self.timeout))
         lock.acquire()
         self.lock = lock
-        self.pool.add((lock, item))
+        self.pool.append((lock, item))
         return item
         
     def __exit__(self, type, value, traceback):
@@ -43,5 +43,5 @@ class Pool:
         key = url+str(timeout)
         if url not in self.pools:
         
-            self.pools[key] = set()
+            self.pools[key] = []
         return ResourceGenerator(self.generate, pool = self.pools[key], timeout=timeout)
