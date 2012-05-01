11172e2b2f00c3810443dd58db1e4cb0784b157a
Colin Rice <colin@daedrum.net>
2012-05-01 11:25:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 
 setup(
     name = "dummy_bithopper_install",
-    version = "0.0.7",
+    version = "0.0.8",
     description = ("A dummy package to install things correctly for bitHopper"),
     install_requires=[
 		'setuptools',
-		'httplib2',
+		'requests',
 		'mechanize',
 		'gevent',
 		'btcnet_info',
