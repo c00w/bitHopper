ed44f1ec9be7d92b07a001459f8c7dae21fe4d5c
Colin Rice <colin@daedrum.net>
2012-08-09 18:56:47 -0400

    Use new btcnet_info version

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup
 
 setup(
     name = "dummy_bithopper_install",
-    version = "0.0.8",
+    version = "0.0.9",
     description = ("A dummy package to install things correctly for bitHopper"),
     install_requires=[
 		'setuptools',
 		'requests',
 		'mechanize',
 		'gevent>=0.13.7',
-		'btcnet_info>=0.1.2.22',
+		'btcnet_info>=0.1.2.23',
 		'flask',
 	],
 )
