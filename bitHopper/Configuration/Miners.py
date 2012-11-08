d2983e4bd714a45fb3186da77eaed02978997aa4
Colin Rice <colin@daedrum.net>
2012-11-08 14:23:23 -0500

    Remove trailing whitespace from miners

--- bitHopper/Configuration/Miners.py
+++ bitHopper/Configuration/Miners.py
@@ -7,31 +7,31 @@ def __patch():
     global miners
     if miners == None:
         miners = load_from_db()
-        
+
 def load_from_db():
     """
     Load miners from database
     """
     columns = [ 'Username TEXT',
                 'Password TEXT']
-    
+
     bitHopper.Database.Commands.Create_Table('Miners', columns)
     results = bitHopper.Database.execute('SELECT Username, Password FROM Miners')
-    
+
     miners = set()
-    
+
     for username, password in results:
         miners.add((username, password))
-        
+
     return miners
-    
+
 def len_miners():
     """
     Returns the length of the worker table
     """
     __patch()
     return len(miners)
-    
+
 def get_miners():
     """
     Returns a list of workers
@@ -40,7 +40,7 @@ def get_miners():
     miners_l = list(miners)
     miners_l.sort()
     return miners_l
-        
+
 def valid(username, password):
     """
     Check if a username, password combination is valid
@@ -48,8 +48,8 @@ def valid(username, password):
     __patch()
     if len(miners) == 0:
         return True
-    return (username, password) in miners    
-    
+    return (username, password) in miners
+
 def add(username, password):
     """
     Adds a miner into the database and the local cache
@@ -58,7 +58,7 @@ def add(username, password):
     if (username, password) not in miners:
         miners.add((username, password))
         bitHopper.Database.execute("INSERT INTO Miners VALUES ('%s','%s')" % (username, password))
-    
+
 def remove(username, password):
     """
     Removes a miner from the local cache and the database
