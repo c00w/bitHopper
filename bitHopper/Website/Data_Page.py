6a3d273238db87b1c4375932731b156abe2c4e4a
Colin Rice <colin@daedrum.net>
2012-11-12 12:45:07 -0500

    Only hide servers if count is over 50million.
    Also show the difficulty percent again

--- bitHopper/Website/Data_Page.py
+++ bitHopper/Website/Data_Page.py
@@ -11,11 +11,12 @@ from flask import Response
 def transform_data(servers):
     for server in servers:
         item = {}
-        for value in ['name', 'shares']:
+        for value in ['name', 'shares', 'coin']:
             if getattr(server, value) != None:
                 item[value] = getattr(server, value)
             else:
                 item[value] = 'undefined'
+        item['shares'] = float(item.get('shares',0)) if item['shares'] != 'undefined' else 'undefined'
         item['payout'] = 0.0
         item['expected_payout'] = 0.0
         item['role'] = 'mine/backup' if server in valid_scheme([server]) else 'info'
@@ -26,7 +27,7 @@ def data():
     response = {}
     response['current'] = ', '.join([s.name for s in get_current_servers()])
     response['mhash'] = get_hashrate() 
-    response['diffs'] = ''
+    response['diffs'] = dict([(coin.name, float(coin.difficulty)) for coin in btcnet_info.get_coins() if coin.difficulty])
     response['sliceinfo'] = None
     response['servers'] = list(transform_data(btcnet_info.get_pools())) 
     response['user'] = None
